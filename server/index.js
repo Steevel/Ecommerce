d02f054c3e91bda34efcf89ee9ecaa2ddb90a4cd
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-07 11:10:34 +0530

    feat - added user, product, cart and order models

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
+
+app.get("/api/test", (req, res) => {
+  console.log("test is successful");
+});
+
+app.use(express.json());
+app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
