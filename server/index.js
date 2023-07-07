58f29b9c627f360889db636bf1a24c69aca027c3
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-07 12:58:37 +0530

    config changes

--- server/index.js
+++ server/index.js
@@ -2,30 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 
 // Connect to DB
-=======
-const userRoute = require("./routes/user");
-
->>>>>>> d02f054c3e91bda34efcf89ee9ecaa2ddb90a4cd
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
-<<<<<<< HEAD
 app.use(express.json());
 app.use("/api/auth", authRoute);
-=======
-app.get("/api/test", (req, res) => {
-  console.log("test is successful");
-});
-
-app.use(express.json());
->>>>>>> d02f054c3e91bda34efcf89ee9ecaa2ddb90a4cd
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
