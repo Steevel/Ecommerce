2752ed25d780139012acba937a910699edfdb0f8
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-07 12:47:23 +0530

    feat - added register feature

--- server/index.js
+++ server/index.js
@@ -2,18 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 
+// Connect to DB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
-app.get("/api/test", (req, res) => {
-  console.log("test is successful");
-});
-
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
