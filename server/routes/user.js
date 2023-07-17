a16149df8721dacc864aab0ffa72c8ae9267e59d
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-17 10:39:04 +0530

    feat - added update user functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,28 @@
+const { verifyTokenAndAuthorization } = require("../middleware/verifyToken");
+const User = require("../models/User");
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("user test is successful");
-});
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
 
-router.post("/userposttest", (req, res) => {
-  const username = req.body.username;
-  console.log(username);
-  res.send(username);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (e) {
+    return res.status(401).json("Your are not authenticated!");
+  }
 });
 
 module.exports = router;
