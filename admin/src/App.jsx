0c71aa3b3a3f54324f2bc615a307ea1bc155645d
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-19 17:32:31 +0530

    feat - Sidebar completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="container">
+        <Sidebar />
+        <div className="other">other pages</div>
+      </div>
     </>
   );
 }
