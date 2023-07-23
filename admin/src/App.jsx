97da47335bd32cbdf1fb3525d498456afcd8ded3
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-23 13:05:55 +0530

    feat - added product page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,6 +6,8 @@ import { Route, Routes } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
           <Route path="/users" element={<UserList />} />
           <Route path="/user/:userId" element={<User />} />
           <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newProduct" element={<NewUser />} />
         </Routes>
       </div>
     </>
