e5ddcfa560dce583281a6939c7ebcfb22ddf92cf
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-24 20:07:51 +0530

    feat - added routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,23 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
+  const user = false;
   return (
     <div>
-      <Home />
-      {/* <ProductList /> */}
-      {/* <Product /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <Cart /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
     </div>
   );
 };
