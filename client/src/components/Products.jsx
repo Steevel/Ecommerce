445259790f07a1331dfc9ef2117c730e53b55aa1
Steevel <57296025+Steevel@users.noreply.github.com>
2023-07-25 16:46:07 +0530

    feat - populated productList

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,6 +1,14 @@
 import { styled } from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const config = {
+  headers: {
+    token: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YjRkMTUwNWM4NGQxNjBmOTU3N2UzZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5MDI3NzI5MCwiZXhwIjoxNjkwNTM2NDkwfQ.WPAx9b82t5INivJBwYMZ5q4ZI7--LH-R73r6E9DG9eE`,
+  },
+};
 
 const Container = styled.div`
   padding: 20px;
@@ -9,12 +17,61 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : "http://localhost:5000/api/products",
+          config
+        );
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((item1, item2) => item1.createdAt - item2.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((item1, item2) => item1.price - item2.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((item1, item2) => item2.price - item1.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 4)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
@@ -14,6 +15,7 @@ mongoose
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
